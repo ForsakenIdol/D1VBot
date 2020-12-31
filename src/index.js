6a424df3ce391253d0451cd013146b964a866def
ForsakenIdol <lachlandwhang@gmail.com>
2020-12-31 22:45:30 +0800

    Removed dotenv and added channel scrape functionality on bot load

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
-const { Message } = require("discord.js");
-const { Discord, dotenv, path } = require("./utils");
+const { Discord, path, getall } = require("./utils");
 
-dotenv.config({path: path.join(__dirname + "/../config.env")});
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   // Configure stuff here!
+  const Guilds = client.guilds.cache;
+  let guild_id, guild_name, guild_channels;
+  [guild_id, guild_name, guild_channels] = Guilds.map(guild => [guild.id, guild.name, guild.channels.cache])[0];
+  console.log(guild_channels.size); // There are 2 hidden channels in Aesthetics.
+
+  // Scrape each channel
+  console.log("Scraping each channel in Aesthetics...");
+  guild_channels.each(channel => {
+    getall(channel, 500)
+    .then(messages => {messages === null ? console.log(`Skipped voice channel ${channel.name}!`) : console.log(`Pulled ${messages.length} messages from the "${channel.name}" channel.`);})
+    .catch(error => {console.log(`\nError while fetching from ${channel.name}!`); console.log(error);});
+  });
+
 });
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() == 'oi d1v') msg.channel.send("Oi what");
+  switch (msg.content.toLowerCase()) {
+    case '--listall -l 300 --force':
+      // Embedded messages are empty and have a length of 0.
+      console.log("Getting messages\n");
+      getall(msg.channel, 300).then(messages => {
+        console.log(messages.length);
+      }).catch(error => {console.log("Error in fetching."); console.log(error);});
+
+      break;
+    case 'rm -rf 20':
+      // Clear the last 20 messages
+      msg.channel.bulkDelete(20);
+      console.log("Cleared 20 messages!");
+      msg.channel.send("Cleared 20 messages. Don't try me bitch.");
+    break;
+    case 'yo d1vbot, you give a fuck?':
+      msg.channel.send("Take a guess (hint: not at all)");
+      break;
+    default:
+      break;
+  }
 });
 
 client.login(process.env.BOTAPITOKEN);
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,25 @@
 const Discord = require('discord.js');
-const dotenv = require('dotenv');
 const path = require('path');
 
-module.exports = { Discord, dotenv, path }
+async function getall(channel, limit = 500) {
+  if (channel.type === 'voice') {
+    console.log(`${channel.name} is a voice channel. Skipping...`);
+    return null;
+  }
+  const sum_messages = [];
+  let last_id;
+  console.log(`Getting a maximum of ${limit} messages from the channel ${channel.name}.`);
+  while (true) {
+      const options = { limit: 100 };
+      if (last_id) options.before = last_id;
+
+      const messages = await channel.messages.fetch(options);
+      sum_messages.push(...messages.array());
+      last_id = messages.last().id;
+
+      console.log(`Pulled ${sum_messages.length} messages...`);
+      if (messages.size != 100 || sum_messages.length >= limit) return sum_messages;
+  }
+} 
+
+module.exports = { Discord, path, getall }
