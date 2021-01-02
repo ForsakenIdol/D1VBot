2781992c09d08c9028ea08ad7160b3ccadc24c71
Lachlan D Whang <lachlandwhang@gmail.com>
2021-01-02 10:59:23 +0800

    Returned limit

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 
 async function getall(channel) {
-  const limit = Math.floor(Number.MAX_SAFE_INTEGER / 100);
+  //const limit = Math.floor(Number.MAX_SAFE_INTEGER / 100);
+  const limit = 500;
 
   if (channel.type === 'voice') {
     console.log(`${channel.name} is a voice channel. Skipping...`);
@@ -12,13 +13,14 @@ async function getall(channel) {
   console.log(`Getting a maximum of ${limit} messages from the channel ${channel.name}.`);
   while (true) {
       const options = { limit: 100 };
-      if (last_id) options.before = last_id;
+      if (last_id) options = { limit: 100, before: last_id }
 
       const messages = await channel.messages.fetch(options);
       // Skip embedded messages - I might be able to use reduce() instead of looping.
       // Maybe instead of skipping embedded messages, check whether the message is embedded and if it is, insert the embedded message with an embedded keyword?
       for (var message in messages.array()) if (message !== undefined) sum_messages.push(message);
-      last_id = sum_messages[sum_messages.length - 1].id;
+      if(sum_messages.length > 0 && last_id === sum_messages[sum_messages.length - 1].id) return sum_messages;
+      else last_id = sum_messages[sum_messages.length - 1].id;
 
       if (sum_messages.length % 1000 === 0) console.log(`Pulled ${sum_messages.length} messages from the ${channel.name} channel...`);
       if (messages.size != 100 || sum_messages.length >= limit) return sum_messages;
